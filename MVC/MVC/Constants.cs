87898f80aae2e8676ad90278994573fb1d18dcdd
Boris Milašinović <boris.milasinovic@fer.hr>
2021-11-12 12:09:51 +0100

    Expression bodies for Constants

--- MVC/MVC/Constants.cs
+++ MVC/MVC/Constants.cs
@@ -2,14 +2,8 @@
 {
   public static class Constants
   {
-    public static string Message
-    {
-      get { return "Message"; }
-    }
+    public static string Message => "Message";
 
-    public static string ErrorOccurred
-    {
-      get { return "ErrorOccurred"; }
-    }
+    public static string ErrorOccurred => "ErrorOccurred";
   }
 }
