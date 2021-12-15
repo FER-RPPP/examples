f1c5ddbd6c7e6b34e71063cb17dc82d83287bb18
Boris Milašinović <boris.milasinovic@fer.hr>
2021-12-15 19:29:09 +0100

    UnitTesting MVC

---
+++ Testing/Testing/MVC.UnitTests/Controllers/AutoComplete/Mjesto.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using MVC.Controllers;
+using MVC.Models;
+using MVC.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC.UnitTests.Controllers.AutoComplete
+{
+  //NOTE: Uses real data!
+  public class Mjesto
+  {    
+    private readonly IOptionsSnapshot<AppSettings> options;
+    private readonly DbContextOptionsBuilder<FirmaContext> dbContextBuilder;
+
+    public Mjesto()
+    {
+      //Arrange
+      var builder = new ConfigurationBuilder()
+                          .AddUserSecrets("Firma");                          
+      var configuration = builder.Build();
+      dbContextBuilder = new DbContextOptionsBuilder<FirmaContext>()
+                            .UseSqlServer(configuration.GetConnectionString("Firma"));
+
+      var mockOptions = new Mock<IOptionsSnapshot<AppSettings>>();
+      var appSettings = new AppSettings
+      {
+        AutoCompleteCount = 10
+      };
+      mockOptions.SetupGet(appsettings => appsettings.Value).Returns(appSettings);
+      options = mockOptions.Object;
+    }
+
+    [Trait("Category", "AutoCompleteController")]
+    [Theory]
+    [InlineData("varaždin", "Varaždin", "Split")]
+    [InlineData("VARAŽDIN", "Varaždin", "Split")]
+    [InlineData("araž", "Varaždin", "Split")]
+    [InlineData("ždin", "Varaždin", "Split")]
+    public async Task ReturnsCity_UsingCaseInsensitiveSubstring(string term, string expectedCity, string unexpectedCity)
+    {      
+      //Act
+      using var ctx = new FirmaContext(dbContextBuilder.Options);
+      var controller = new AutoCompleteController(ctx, options);
+      IEnumerable<IdLabel> result = await controller.Mjesto(term);
+
+      //Assert      
+      result.Any(l => l.Label.EndsWith(expectedCity)).Should().BeTrue();
+      result.Any(l => l.Label.EndsWith(unexpectedCity)).Should().BeFalse();
+    }
+
+    [Trait("Category", "AutoCompleteController")]
+    [Theory]
+    [InlineData("a", 20)]    
+    [InlineData("a", 50)]
+    [InlineData("a", 0)]
+    public async Task Returns_SubsetOfAllCities(string term, int count)
+    {
+      //Arrange
+      var mockOptions = new Mock<IOptionsSnapshot<AppSettings>>();
+      var appSettings = new AppSettings
+      {
+        AutoCompleteCount = count
+      };
+      mockOptions.SetupGet(appsettings => appsettings.Value).Returns(appSettings);
+      
+      //Act
+      using var ctx = new FirmaContext(dbContextBuilder.Options);
+      var controller = new AutoCompleteController(ctx, mockOptions.Object);
+      IEnumerable<IdLabel> result = await controller.Mjesto(term);
+
+      //Assert      
+      result.Count().Should().Be(count);      
+    }
+  }
+}
---
+++ Testing/Testing/MVC.UnitTests/Controllers/Mjesto/Create.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using MVC.Controllers;
+using MVC.Models;
+using MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC.UnitTests.Controllers.Mjesto
+{
+  public class Create
+  {
+    private readonly IOptionsSnapshot<AppSettings> options;        
+
+    public Create()
+    {      
+      var mockOptions = new Mock<IOptionsSnapshot<AppSettings>>();
+      var appSettings = new AppSettings
+      {
+        PageSize = 10
+      };
+      mockOptions.SetupGet(appsettings => appsettings.Value).Returns(appSettings);
+      options = mockOptions.Object;
+    }
+
+    [Fact]
+    [Trait("Category", "CitiesController")]
+    public async Task PreparesViewBag()
+    {
+      int totalNumber = 50;
+      // Arrange            
+      var dbOptions = new DbContextOptionsBuilder<FirmaContext>()
+                          .UseInMemoryDatabase(databaseName: nameof(PreparesViewBag))
+                          .Options;
+
+      using var context = new FirmaContext(dbOptions);
+      await AddCountries(context, totalNumber);
+
+      var controller = new MjestoController(context, options);      
+
+      // Act
+      var result = controller.Create();
+
+      // Assert
+      var task = Assert.IsType<Task<IActionResult>>(result);
+      IActionResult actionResult = await task;
+      var viewResult = Assert.IsType<ViewResult>(actionResult);
+      viewResult.Model.Should().BeNull();
+
+      var countries = viewResult.ViewData["Drzave"];
+      countries.Should()
+               .NotBeNull(because: "controller should prepare data for drop down list")
+               .And.BeOfType<SelectList>()
+               .Which.Count().Should().Be(totalNumber);      
+    }
+    
+    private async Task AddCountries(FirmaContext context, int count)
+    {
+      for (int i = 0; i < count; i++)
+      {
+        string id = $"{i:D2}";
+        context.Add(new Models.Drzava
+        {
+          OznDrzave = id,
+          NazDrzave = id
+        });
+      }
+      await context.SaveChangesAsync();
+    }
+  }
+}
