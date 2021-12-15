fee7454fdf9bb26e3aec86dbf0e861201bfb2dfa
Boris Milašinović <boris.milasinovic@fer.hr>
2021-12-15 20:46:47 +0100

    MVC integration tests

---
+++ Testing/Testing/MVC.IntegrationTests/CountryControllerShould.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MVC.IntegrationTests
+{
+  public class CountryControllerShould : IClassFixture<CustomWebApplicationFactory<CountryControllerShould>>
+  {
+    private readonly CustomWebApplicationFactory<CountryControllerShould> factory;
+
+    public CountryControllerShould(CustomWebApplicationFactory<CountryControllerShould> factory)
+    {
+      this.factory = factory;
+    }
+
+    [Fact]
+    [Trait("Category", "Integration Tests")]    
+    public async Task Redirect_When_DbContainsNoCountries()
+    {      
+      string url = "/Drzava/Index";
+      var client = factory.CreateClient(new WebApplicationFactoryClientOptions
+      {
+        AllowAutoRedirect = false
+      });
+      var response = await client.GetAsync(url);
+      response.StatusCode.Should().Be(HttpStatusCode.Redirect);
+      response.Headers.Location
+        .Should().NotBeNull()
+        .And.Be("/Drzava/Create");
+    }   
+  }
+}
---
+++ Testing/Testing/MVC.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.IntegrationTests
+{
+  public class CustomWebApplicationFactory<TTest> : WebApplicationFactory<Startup> 
+  {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+      builder.ConfigureServices(services =>
+      {
+        #region Replace database context with an in-memory context
+        var descriptor = services.SingleOrDefault(
+            d => d.ServiceType ==
+                typeof(DbContextOptions<FirmaContext>));
+        services.Remove(descriptor);
+
+        services.AddDbContext<FirmaContext>(options =>
+        {
+          options.UseInMemoryDatabase("Firma-" + typeof(TTest).Name); //database per test collection          
+        });
+
+        var sp = services.BuildServiceProvider();
+
+        using (var scope = sp.CreateScope())
+        {
+          var scopedServices = scope.ServiceProvider;
+          var db = scopedServices.GetRequiredService<FirmaContext>();          
+          db.Database.EnsureCreated();         
+        }
+        #endregion        
+      });
+    }    
+  }
+}
