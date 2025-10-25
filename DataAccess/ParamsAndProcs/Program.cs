b6b0efa4c3b6c013996fbfeb012764d30bc583ce
Boris Milašinović <boris.milasinovic@fer.hr>
2025-10-25 20:46:03 +0200

    Packages upgrade. Fix nullable references warnings.

--- DataAccess/ParamsAndProcs/Program.cs
+++ DataAccess/ParamsAndProcs/Program.cs
@@ -1,132 +1,151 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.ComponentModel.Design;
 using System.Data.Common;
 
-namespace ParamsAndProcs
+namespace ParamsAndProcs;
+
+public class Program
 {
-  public class Program
-  {
-    static IConfigurationRoot? config;
-    static DbProviderFactory factory = SqlClientFactory.Instance;
-    //static DbProviderFactory factory = DbProviderFactories.GetFactory("Microsoft.Data.SqlClient"); //can be used if registered before using DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance)
+  static IConfigurationRoot config = new ConfigurationBuilder()
+                                          .AddJsonFile("appsettings.json")
+                                          .AddUserSecrets<Program>()
+                                          .Build();
+  static DbProviderFactory factory = SqlClientFactory.Instance;
+  //static DbProviderFactory factory = DbProviderFactories.GetFactory("Microsoft.Data.SqlClient"); //can be used if registered before using DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance)
 
-    public static void Main(string[] args)
+  public static void Main(string[] args)
+  {   
+    ParametrizedQueryDemo();
+
+    Console.WriteLine("Press ENTER to continue");
+    Console.ReadLine();
+
+    if (decimal.TryParse(config["PriceThreshold"], out decimal threshold)) 
     {
-      config = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .AddUserSecrets<Program>()
-                    .Build();
-
-      ParametrizedQueryDemo();
-
-      Console.WriteLine("Press ENTER to continue");
-      Console.ReadLine();
-
-      if (decimal.TryParse(config["PriceThreshold"], out decimal threshold)) 
-      {
-        ProcedureDemo(threshold);
-      }
-      else
-      {
-        Console.WriteLine("Invalid value for PriceThreshold configuration key");
-      }
-      
+      ProcedureDemo(threshold);
     }
+    else
+    {
+      Console.WriteLine("Invalid value for PriceThreshold configuration key");
+    }
+    
+  }
 
-    /// <summary>
-    /// Uses DBProviderFactory to create connection and other database related objects
-    /// SQL Query is parametrized and returns two results sets
-    /// </summary>
-    private static void ParametrizedQueryDemo()
-    {     
-      using (DbConnection? conn = factory.CreateConnection())
+  /// <summary>
+  /// Uses DBProviderFactory to create connection and other database related objects
+  /// SQL Query is parametrized and returns two results sets
+  /// </summary>
+  private static void ParametrizedQueryDemo()
+  {     
+    using (DbConnection? conn = factory.CreateConnection())
+    {
+      if (conn == null)
       {
-        conn.ConnectionString = config.GetConnectionString("Firma");
-        using (DbCommand command = factory.CreateCommand())
-        {
-          command.Connection = conn;
-          command.CommandText = @"SELECT TOP 3 NazArtikla, CijArtikla FROM Artikl WHERE JedMjere = @JedMjere ORDER BY CijArtikla DESC;
-                                  SELECT TOP 3 NazArtikla, CijArtikla FROM Artikl WHERE JedMjere = @JedMjere AND CijArtikla > @Cijena ORDER BY CijArtikla";
-          DbParameter param = command.CreateParameter();
-          param.ParameterName = "JedMjere";
-          param.DbType = System.Data.DbType.String;
-          param.Value = "kom";
-          command.Parameters.Add(param);
+        Console.WriteLine("Connection could not be created");
+        return;
+      }
 
-          param = factory.CreateParameter();
-          param.ParameterName = "Cijena";
-          param.DbType = System.Data.DbType.Decimal;
-          param.Value = 100m;
-          command.Parameters.Add(param);
-          conn.Open();
-          using (DbDataReader reader = command.ExecuteReader())
-          {
-            bool firstResult = true;
-            do
-            {
-              Console.WriteLine(firstResult ? "The most expensive products sold in pieces:" : "The cheapest products above threashold sold in pieces");
-              Console.WriteLine("-----------------------------");
-              while (reader.Read())
-              {
-                Console.WriteLine("\t{0} - {1}", reader["NazArtikla"], reader["CijArtikla"]);
-              }
-              Console.WriteLine();
-              firstResult = false;
-            }
-            while (reader.NextResult());
-          }
+      conn.ConnectionString = config.GetConnectionString("Firma");
+      using (DbCommand? command = factory.CreateCommand())
+      {
+        if (command == null)
+        {
+          Console.WriteLine("Command could not be created");
+          return;
         }
-      }
-    }
+        command.Connection = conn;
+        command.CommandText = @"SELECT TOP 3 NazArtikla, CijArtikla FROM Artikl WHERE JedMjere = @JedMjere ORDER BY CijArtikla DESC;
+                                  SELECT TOP 3 NazArtikla, CijArtikla FROM Artikl WHERE JedMjere = @JedMjere AND CijArtikla > @Cijena ORDER BY CijArtikla";
+        DbParameter param = command.CreateParameter();
+        param.ParameterName = "JedMjere";
+        param.DbType = System.Data.DbType.String;
+        param.Value = "kom";
+        command.Parameters.Add(param);
 
-    private static void ProcedureDemo(decimal priceThreshold)
-    {      
-      using (DbConnection conn = factory.CreateConnection())
-      {
-        conn.ConnectionString = config.GetConnectionString("Firma");
-        using (DbCommand command = factory.CreateCommand())
+        param = command.CreateParameter();
+        param.ParameterName = "Cijena";
+        param.DbType = System.Data.DbType.Decimal;
+        param.Value = 100m;
+        command.Parameters.Add(param);
+        conn.Open();
+        using (DbDataReader reader = command.ExecuteReader())
         {
-          command.Connection = conn;
-          command.CommandText = "ap_ArtikliSkupljiOd";
-          command.CommandType = System.Data.CommandType.StoredProcedure;
-
-          DbParameter param = command.CreateParameter();
-          param.ParameterName = "Prag";
-          param.DbType = System.Data.DbType.Decimal;
-          param.Value = priceThreshold;
-          command.Parameters.Add(param);
-
-          param = command.CreateParameter();
-          param.ParameterName = "BrojSkupljih";
-          param.DbType = System.Data.DbType.Int32;
-          param.Direction = System.Data.ParameterDirection.Output;
-
-          command.Parameters.Add(param);
-
-          param = command.CreateParameter();
-          param.ParameterName = "BrojJeftinijih";
-          param.DbType = System.Data.DbType.Int32;
-          param.Direction = System.Data.ParameterDirection.Output;
-          command.Parameters.Add(param);
-
-          conn.Open();
-          using (DbDataReader reader = command.ExecuteReader())
+          bool firstResult = true;
+          do
           {
-            Console.WriteLine("Products with the price above the threashold ({0})", priceThreshold);
-            Console.WriteLine("---------------------------------");
+            Console.WriteLine(firstResult ? "The most expensive products sold in pieces:" : "The cheapest products above threashold sold in pieces");
+            Console.WriteLine("-----------------------------");
             while (reader.Read())
             {
               Console.WriteLine("\t{0} - {1}", reader["NazArtikla"], reader["CijArtikla"]);
             }
-            Console.WriteLine("---------");
+            Console.WriteLine();
+            firstResult = false;
           }
-          //when we are finished with reader then we can read parameters values
-          Console.WriteLine("Above threashold: {0}, Below threashold: {1}",
-              command.Parameters["BrojSkupljih"].Value, 
-              command.Parameters["BrojJeftinijih"].Value);
+          while (reader.NextResult());
         }
+      }
+    }
+  }
+
+  private static void ProcedureDemo(decimal priceThreshold)
+  {      
+    using (DbConnection? conn = factory.CreateConnection())
+    {
+      if (conn == null)
+      {
+        Console.WriteLine("Connection could not be created");
+        return;
+      }
+
+      conn.ConnectionString = config.GetConnectionString("Firma");
+      using (DbCommand? command = factory.CreateCommand())
+      {
+        if (command == null)
+        {
+          Console.WriteLine("Command could not be created");
+          return;
+        }
+
+        command.Connection = conn;
+        command.CommandText = "ap_ArtikliSkupljiOd";
+        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+        DbParameter param = command.CreateParameter();
+        param.ParameterName = "Prag";
+        param.DbType = System.Data.DbType.Decimal;
+        param.Value = priceThreshold;
+        command.Parameters.Add(param);
+
+        param = command.CreateParameter();
+        param.ParameterName = "BrojSkupljih";
+        param.DbType = System.Data.DbType.Int32;
+        param.Direction = System.Data.ParameterDirection.Output;
+
+        command.Parameters.Add(param);
+
+        param = command.CreateParameter();
+        param.ParameterName = "BrojJeftinijih";
+        param.DbType = System.Data.DbType.Int32;
+        param.Direction = System.Data.ParameterDirection.Output;
+        command.Parameters.Add(param);
+
+        conn.Open();
+        using (DbDataReader reader = command.ExecuteReader())
+        {
+          Console.WriteLine("Products with the price above the threashold ({0})", priceThreshold);
+          Console.WriteLine("---------------------------------");
+          while (reader.Read())
+          {
+            Console.WriteLine("\t{0} - {1}", reader["NazArtikla"], reader["CijArtikla"]);
+          }
+          Console.WriteLine("---------");
+        }
+        //when we are finished with reader then we can read parameters values
+        Console.WriteLine("Above threashold: {0}, Below threashold: {1}",
+            command.Parameters["BrojSkupljih"].Value, 
+            command.Parameters["BrojJeftinijih"].Value);
       }
     }
   }
